29f373c89cd7500baa0a638102b2ccb10740ef9b
ManuOSMx <manuel4612@hotmail.com>
2023-04-22 03:17:03 -0600

    Firebase integration OK

---
+++ src/assets/js/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAYDVKJm843NzLI98DKa5-cegqw6VnkdWk",
+    authDomain: "tupu-86fcc.firebaseapp.com",
+    projectId: "tupu-86fcc",
+    storageBucket: "tupu-86fcc.appspot.com",
+    messagingSenderId: "522793879122",
+    appId: "1:522793879122:web:97e1e9eed385b34ab4018b",
+    measurementId: "G-75M74687P6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const getMembers = () => getDocs(collection(db, "equipo"));
